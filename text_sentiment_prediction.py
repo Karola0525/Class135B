44bd86fdfd0221bb6f627a409e13854f161ffbe1
Karola0525 <114681978+Karola0525@users.noreply.github.com>
2024-01-11 14:27:09 -0500

    Update text_sentiment_prediction.py

--- text_sentiment_prediction.py
+++ text_sentiment_prediction.py
@@ -7,14 +7,14 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 
 
-train_data = pd.read_csv("/content/PRO-1-4-C134-Codigo-referencia/static/assets/data_files/tweet_emotions.csv")    
+train_data = pd.read_csv("/content/Class135B/static/assets/data_files/tweet_emotions.csv")    
 training_sentences = []
 
 for i in range(len(train_data)):
     sentence = train_data.loc[i, "content"]
     training_sentences.append(sentence)
 
-model = load_model("/content/PRO-1-4-C134-Codigo-referencia/static/assets/model_files/Tweet_Emotion.h5")
+model = load_model("/content/Class135B/static/assets/model_files/Tweet_Emotion.h5")
 
 vocab_size = 40000
 max_length = 100
@@ -26,19 +26,19 @@ tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
 tokenizer.fit_on_texts(training_sentences)
 
 emo_code_url = {
-    "empty": [0, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Empty.png"],
-    "sadness": [1,"https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Sadness.png" ],
-    "enthusiasm": [2, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Enthusiastic.png"],
-    "neutral": [3, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Neutral.png"],
-    "worry": [4, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Worry.png"],
-    "surprise": [5, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Surprise.png"],
-    "love": [6, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Love.png"],
-    "fun": [7, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Fun.png"],
-    "hate": [8, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Hate.png"],
-    "happiness": [9, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Happiness.png"],
-    "boredom": [10, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Boredom.png"],
-    "relief": [11, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Relief.png"],
-    "anger": [12, "https://raw.githubusercontent.com/BYJUS-smah/PRO-1-4-C134-Codigo-referencia/main/static/assets/emoticons/Anger.png"]
+    "empty": [0, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Empty.png"],
+    "sadness": [1,"https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Sadness.png" ],
+    "enthusiasm": [2, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Enthusiastic.png"],
+    "neutral": [3, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Neutral.png"],
+    "worry": [4, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Worry.png"],
+    "surprise": [5, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Surprise.png"],
+    "love": [6, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Love.png"],
+    "fun": [7, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Fun.png"],
+    "hate": [8, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Hate.png"],
+    "happiness": [9, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Happiness.png"],
+    "boredom": [10, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Boredom.png"],
+    "relief": [11, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Relief.png"],
+    "anger": [12, "https://raw.githubusercontent.com/BYJUS-smah/Class135B/main/static/assets/emoticons/Anger.png"]
     
     }
 
